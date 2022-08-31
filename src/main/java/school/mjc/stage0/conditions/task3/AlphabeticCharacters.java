e7ebc74619670ebacbc5e9f517a9c5145d886622
monsterJohan <nikfil97@gmail.com>
2022-08-31 16:24:37 +0200

    Implemented Task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String vowelString = "AaEeIiOoUuYy";
+        if (character >= 65 && character <= 90 || character >= 97 && character <= 122) {
+            if (vowelString.indexOf(character) != -1) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary >= 0 && salary <= 10000) {
+            System.out.println((float)0.85 * salary);
+        } else if (salary > 10000 && salary <= 20000) {
+            System.out.println((float)0.82 * salary);
+        } else if (salary > 20000) {
+            System.out.println((float)0.80 * salary);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
